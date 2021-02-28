785582c9d6b16627d65430926cfdb22cc2563ebc
Adam Bekila <adam.b@datatonic.com>
2021-02-28 17:40:41 +0000

    docs(updating docs): rEADME file update

--- modules/firewall/ouput.tf
+++ modules/firewall/ouput.tf
@@ -1,6 +1,6 @@
 output "firewall_rule_id" {
   value       = [for u in google_compute_firewall.dynamic_firewall_rules : u.id]
-  description = "The created firewall rule resources"
+  description = "The created firewall rule resources."
 }
 
 output "rule_url" {
